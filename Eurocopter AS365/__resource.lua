f69d73d13deb08c56910c314259042dc59542ef1
rhys19 <30848481+rhys19@users.noreply.github.com>
2018-09-16 22:18:26 -0500

    Add files via upload

---
+++ Eurocopter AS365/__resource.lua
@@ -0,0 +1,9 @@
+resource_manifest_version '77731fab-63ca-442c-a67b-abc70f28dfa5'
+
+files {
+    'data/vehicles.meta',
+    'data/handling.meta',
+}
+
+data_file 'HANDLING_FILE' 'data/handling.meta'
+data_file 'VEHICLE_METADATA_FILE' 'data/vehicles.meta'
